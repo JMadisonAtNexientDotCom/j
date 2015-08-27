93aaeca3868f43615af7ac365de29b7c901ee6a7
John Mark Isaac Madison <JMadison@Nexient.com>
2015-08-27 16:27:39 -0400

    Will .java class without static void main crash app? We will see.

---
+++ src/main/java/com/myPak/SomeEntryPoint.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.myPak;
+
+/**
+ * Making a class without a static main, to see if we can push this
+ * to openshift without getting application to crash.
+ * @author jmadison
+ */
+public class SomeEntryPoint {
+    public SomeEntryPoint(){
+        System.out.println("Hello there");
+    }
+}
