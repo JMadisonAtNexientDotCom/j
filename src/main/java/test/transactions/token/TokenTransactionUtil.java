1bdb7c8e49328008ec6b09858806f2f7cd5a891a
John Mark Isaac Madison <JMadison@Nexient.com>
2015-09-02 20:33:42 -0400

    getTokenEntityUsingHash, added, but not tested.

--- src/main/java/test/transactions/token/TokenTransactionUtil.java
+++ src/main/java/test/transactions/token/TokenTransactionUtil.java
@@ -2,9 +2,9 @@ package test.transactions.token;
 
 import org.hibernate.Criteria;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
+//import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Restrictions;
-import utils.HibernateUtil;
+//import utils.HibernateUtil;
 import test.entities.TokenTable;
 import test.transactions.util.TransUtil;
 import test.entities.BaseEntityContainer;
@@ -35,31 +35,52 @@ public class TokenTransactionUtil {
     }//FUNC::END
     
      
-    /** Gets an entity representing a [record/entry] in the token table.     ---
-    --- Using the token's value to fetch the [record/entry].                 ---
-    --- @param tv : The token's value.                                       **/
-    public static BaseEntityContainer getTokenEntityUsingTokenValue(String tv){
+    /** Gets an entity representing a [record/entry] in the token table.     ***
+    *** Using the token's HASH value to fetch the [record/entry].            ***
+    *** @param tv : The token's value.                                       ***
+    *** @return   : Returns container with found entity.                     *** 
+    ***             If no match found, container is configured               ***
+    ***             to reflect that. This helps us avoid returning NULL.     **/
+    public static BaseEntityContainer getTokenEntityUsingHash(String tv){
         
         //Enter Transaction:
         Session session = TransUtil.enterTransaction();
         
         //Transaction Logic:
-        Criteria criteria = session.createCriteria(Employee.class);
-        criteria.add(Restrictions.eq("id", employeeId));
+        Criteria criteria = session.createCriteria(TokenTable.class);
+        criteria.add(Restrictions.eq("value", tv));
 
-        Employee employee = (Employee) criteria.uniqueResult();
+        TokenTable theToken = (TokenTable) criteria.uniqueResult();
 
-        if (employee!=null) {
-                System.out.println("Employee found:");
-                System.out.println(employee.getId() + " - " + employee.getName());
+        if (theToken!=null) {
+                System.out.println("theToken found:");
+                System.out.println(theToken.getValue()+ " - " 
+                                                       + theToken.getComment());
         }
         
         //Exit Transaction:
-        TransUtil.exitTransaction(session, ent);
-    }
+        TransUtil.exitTransaction(session, theToken);
+        
+        //Create output:
+        BaseEntityContainer op = new BaseEntityContainer();
+        if(null == theToken)
+        {   //make container configured as empty:
+            op.entity = null;
+            op.exists = false;
+        }
+        else
+        {   //make container configured as populated:
+            op.entity = theToken;
+            op.exists = true;
+        }
+        
+        //return the container with our result of query:
+        return op;
+        
+    }//END::FUNC
     
     public static void getAllTokens(){
-        throw new MyError("TODO: getAllTokens function.")
+        throw new MyError("TODO: getAllTokens function.");
     }
     
 }//CLASS::END
