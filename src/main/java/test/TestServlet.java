f28adced1f15d373f77c2f931f162b6832cd6589
John Mark Isaac Madison <JMadison@Nexient.com>
2015-09-02 20:59:08 -0400

    TokenHardCodedFetchServlet added, not tested.

--- src/main/java/test/TestServlet.java
+++ src/main/java/test/TestServlet.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package test;
 
 import java.io.File;
@@ -55,4 +50,4 @@ public class TestServlet extends HttpServlet{
      private static String getTestConfigMSG(){
          return TestConfig.testVar01;
      }
-}
+}//CLASS::END
