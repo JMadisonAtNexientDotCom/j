f1ed541c66828b7b87b6eeb23c5326e149ff6277
John Mark Isaac Madison <JMadison@Nexient.com>
2015-09-03 15:25:46 -0400

    STOP ADDING STUFF TO MY POM.XML NET BEANS!!!

---
+++ src/main/java/test/servlets/rest/TokenRestService.java
@@ -0,0 +1,21 @@
+package test.servlets.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+ 
+@Path("/TokenRestService")
+public class TokenRestService {
+ 
+	@GET
+	@Path("/{param}")
+	public Response getMsg(@PathParam("param") String msg) {
+ 
+		String output = "Servlet: TokenRestService : " + msg;
+ 
+		return Response.status(200).entity(output).build();
+ 
+	}
+ 
+}
