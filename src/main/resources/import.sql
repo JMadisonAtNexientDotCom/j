58117f60a50b93a5bd057d71ab5805274267b49b
John Mark Isaac Madison <JMadison@Nexient.com>
2015-09-09 13:32:37 -0400

    Attempting LAST_INSERT_ID() assign to var

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -20,9 +20,9 @@ CREATE TABLE riddle_rhyme_wrong_table (riddle_id INT UNSIGNED NOT NULL, rhyme_id
 
 -- RIDDLE + RHYME ENTRY --
 INSERT INTO riddle_table (string) VALUES ('I build up castles but tear down mountains make some men blind but others to see. What am I?');
+SET @last_riddle_id := LAST_INSERT_ID();
 INSERT INTO rhyme_table (string) VALUES ('Sand my man.');
-SET @last_riddle_id := 1;
-SET @last_rhyme_id := 1;
+SET @last_rhyme_id := LAST_INSERT_ID();
 INSERT INTO riddle_rhyme_truth_table(riddle_id, rhyme_id) VALUES (@last_riddle_id, @last_rhyme_id);
 
 COMMIT;
