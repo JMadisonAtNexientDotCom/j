905c9d785917e6cf62e326806945d00b9e8dcb65
John Mark Isaac Madison <JMadison@Nexient.com>
2015-09-09 12:20:09 -0400

    PREV_COMMIT_BROKEN: LAST_INSERT_ID() fails?

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -58,58 +58,58 @@ CREATE TABLE riddle_rhyme_wrong_table (riddle_id INT UNSIGNED NOT NULL,
 
 -- RIDDLE + RHYME ENTRY --
 INSERT INTO riddle_table (string) VALUES ('I build up castles but tear down mountains make some men blind but others to see. What am I?');
-SET @last_riddle_id = LAST_INSERT_ID();
+SET @last_riddle_id = 1;
 INSERT INTO rhyme_table (string) VALUES ('Sand my man.');
-SET @last_rhyme_id  = LAST_INSERT_ID();
+SET @last_rhyme_id  = 1;
 INSERT INTO riddle_rhyme_truth_table(riddle_id, rhyme_id) VALUES (@last_riddle_id,@last_rhyme_id);
 
 -- RIDDLE + RHYME ENTRY --
 INSERT INTO riddle_table (string) VALUES ('I have lots to say but can never speak. Knowledge is the thing I eat. Inside me adventures you will find quests and treasures of every kind for all those that wish to visit me your hands are the ultimate key. What am I?');
-SET @last_riddle_id = LAST_INSERT_ID();
+SET @last_riddle_id = 2;
 INSERT INTO rhyme_table (string) VALUES ('A book, Ms. Cook.');
-SET @last_rhyme_id  = LAST_INSERT_ID();
+SET @last_rhyme_id  = 2;
 INSERT INTO riddle_rhyme_truth_table(riddle_id, rhyme_id) VALUES (@last_riddle_id,@last_rhyme_id);
 
 -- RIDDLE + RHYME ENTRY --
 INSERT INTO riddle_table (string) VALUES ('The beginning of eternity, the end of time and space, the beginnings of every end, the end of every place. What am I?');
-SET @last_riddle_id = LAST_INSERT_ID();
+SET @last_riddle_id = 3;
 INSERT INTO rhyme_table (string) VALUES ('The letter "e" I see.');
-SET @last_rhyme_id  = LAST_INSERT_ID();
+SET @last_rhyme_id  = 3;
 INSERT INTO riddle_rhyme_truth_table(riddle_id, rhyme_id) VALUES (@last_riddle_id,@last_rhyme_id);
 
 -- RIDDLE + RHYME ENTRY --
 INSERT INTO riddle_table (string) VALUES ('White flesh above and brown gills below. Never moving an inch, and in darkness I grow.');
-SET @last_riddle_id = LAST_INSERT_ID();
+SET @last_riddle_id = 4;
 INSERT INTO rhyme_table (string) VALUES ('A mushroom maybe?');
-SET @last_rhyme_id  = LAST_INSERT_ID();
+SET @last_rhyme_id  = 4;
 INSERT INTO riddle_rhyme_truth_table(riddle_id, rhyme_id) VALUES (@last_riddle_id,@last_rhyme_id);
 
 -- RIDDLE + RHYME ENTRY --
 INSERT INTO riddle_table (string) VALUES ('I touch the Earth, I touch the sky, But if I touch you, you will surely die.');
-SET @last_riddle_id = LAST_INSERT_ID();
+SET @last_riddle_id = 5;
 INSERT INTO rhyme_table (string) VALUES ('Lightning is frightening.');
-SET @last_rhyme_id  = LAST_INSERT_ID();
+SET @last_rhyme_id  = 5;
 INSERT INTO riddle_rhyme_truth_table(riddle_id, rhyme_id) VALUES (@last_riddle_id,@last_rhyme_id);
 
 -- RIDDLE + RHYME ENTRY --
 INSERT INTO riddle_table (string) VALUES ('I run away every day and return every night. I am the bane of the flower and a friend to furry little robbers.');
-SET @last_riddle_id = LAST_INSERT_ID();
+SET @last_riddle_id = 6;
 INSERT INTO rhyme_table (string) VALUES ('Raccoons after noon.');
-SET @last_rhyme_id  = LAST_INSERT_ID();
+SET @last_rhyme_id  = 6;
 INSERT INTO riddle_rhyme_truth_table(riddle_id, rhyme_id) VALUES (@last_riddle_id,@last_rhyme_id);
 
 -- RIDDLE + RHYME ENTRY --
 INSERT INTO riddle_table (string) VALUES ('I live in the oceans, the rivers and seas, when I am cold I float, when I am hot, I am free.');
-SET @last_riddle_id = LAST_INSERT_ID();
+SET @last_riddle_id = 7;
 INSERT INTO rhyme_table (string) VALUES ('Water is wet.');
-SET @last_rhyme_id  = LAST_INSERT_ID();
+SET @last_rhyme_id  = 7;
 INSERT INTO riddle_rhyme_truth_table(riddle_id, rhyme_id) VALUES (@last_riddle_id,@last_rhyme_id);
 
 -- RIDDLE + RHYME ENTRY --
 INSERT INTO riddle_table (string) VALUES ('You use it between your head and toes, the more it works the thinner it grows. What is it?');
-SET @last_riddle_id = LAST_INSERT_ID();
+SET @last_riddle_id = 8;
 INSERT INTO rhyme_table (string) VALUES ('Soap on a rope.');
-SET @last_rhyme_id  = LAST_INSERT_ID();
+SET @last_rhyme_id  = 8;
 INSERT INTO riddle_rhyme_truth_table(riddle_id, rhyme_id) VALUES (@last_riddle_id,@last_rhyme_id);
 
 
