462490858cf341e50a35820b0571226d978226b7
John Mark Isaac Madison <JMadison@Nexient.com>
2015-09-09 13:14:44 -0400

    Slowly re-add PREV_COMMIT WORKED.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,6 +2,9 @@
 
 BEGIN;
 
+SET @last_riddle_id = 0;
+SET @last_rhyme_id = 0;
+
 DROP TABLE IF EXISTS riddle_table;
 DROP TABLE IF EXISTS rhyme_table;
 DROP TABLE IF EXISTS riddle_rhyme_truth_table;
@@ -11,7 +14,9 @@ DROP TABLE IF EXISTS riddle_rhyme_wrong_table;
 -- Max length == 80 (one line) for our answers (rhymes).              --
 CREATE TABLE riddle_table (id serial PRIMARY KEY, string varchar(800) );
 CREATE TABLE rhyme_table (id serial PRIMARY KEY, string varchar(80) );
-CREATE TABLE riddle_rhyme_truth_table (riddle_id INT UNSIGNED NOT NULL, rhyme_id  INT UNSIGNED NOT NULL);
+CREATE TABLE riddle_rhyme_truth_table (riddle_id INT UNSIGNED NOT NULL, rhyme_id INT UNSIGNED NOT NULL);
+CREATE TABLE riddle_rhyme_wrong_table (riddle_id INT UNSIGNED NOT NULL, rhyme_id  INT UNSIGNED NOT NULL);
+
 
 -- RIDDLE + RHYME ENTRY --
 INSERT INTO riddle_table (string) VALUES ('I build up castles but tear down mountains make some men blind but others to see. What am I?');
