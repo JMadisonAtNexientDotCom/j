4082b962cb587da9a9720f505d6df6b5868cba7b
John Mark Isaac Madison <JMadison@Nexient.com>
2015-09-10 13:31:52 -0400

    SAFTEY_SAVE VIM_PROBLEMS in last commit

--- src/main/java/test/servlets/rest/riddleRhyme/RiddleRhymeRestService.java
+++ src/main/java/test/servlets/rest/riddleRhyme/RiddleRhymeRestService.java
@@ -6,8 +6,7 @@ import test.config.constants.ServletClassNames;
 /** A rest service class that handles any api calls involving our
  *  1: riddle table (questions)
  *  2: rhyme  table (answers)
- * @author jmadison
- */
+ * @author jmadison **/
 @Path(ServletClassNames.RiddleRhymeRestService_MAPPING)
 public class RiddleRhymeRestService {
     
