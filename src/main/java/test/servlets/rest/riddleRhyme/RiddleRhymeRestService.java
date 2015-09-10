33148dca0ae680094eb79eb578864ce753dde8d7
John Mark Isaac Madison <JMadison@Nexient.com>
2015-09-10 13:51:36 -0400

    Maybe add /ping method to all rest services?

--- src/main/java/test/servlets/rest/riddleRhyme/RiddleRhymeRestService.java
+++ src/main/java/test/servlets/rest/riddleRhyme/RiddleRhymeRestService.java
@@ -1,6 +1,9 @@
 package test.servlets.rest.riddleRhyme;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
 import test.config.constants.ServletClassNames;
 
 /** A rest service class that handles any api calls involving our
@@ -9,5 +12,24 @@ import test.config.constants.ServletClassNames;
  * @author jmadison **/
 @Path(ServletClassNames.RiddleRhymeRestService_MAPPING)
 public class RiddleRhymeRestService {
+    
+    /** Been duplicating this code a lot. Maybe we can put this in a base
+     *  class and have it called "getTestMessage" ?
+     *  It can be used basically as a ping.. Maybe it should be called...
+     *  "ping" Since that is the usage. A basic connectivity test.
+     *  Can you get back the message you sent via a ping?
+     * @param msg
+     * @return 
+     */
+    @GET
+    @Path("getMsg/{param}")
+    public Response getMsg(@PathParam("param") String msg) {
+
+        String output = "Servlet: RiddleRhymeRestService : " + msg;
+
+        return Response.status(200).entity(output).build();
+
+    }//FUNC::END
+    
     
 }//CLASS::END
