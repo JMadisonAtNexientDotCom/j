ac696af86114be645d001f3f2f25a661837b2979
John Mark Isaac Madison <JMadison@Nexient.com>
2015-09-11 22:17:38 -0400

    WORKING_COMMIT. Build a deck to attack the ninja.

---
+++ src/main/java/test/entities/composites/Deck.java
@@ -0,0 +1,22 @@
+package test.entities.composites;
+
+import java.util.List;
+
+/** A deck is a collection of CueCards. It represents a barrage of questions
+ *  that the jester is about to ask the ninja. 
+ * 
+ *  Think of a jester with a deck of cards.
+ *  But instead of playing cards, they are CueCards with riddles on them.
+ * @author jmadison                                                          **/
+public class Deck {
+    
+    /** The cards in the deck that are about to 
+     *  barrage(attack, mentally) the ninja. **/
+    public List<CueCard> Cards;
+    
+    //TODO: An object giving us stats on the deck.
+    //      Think of it like a "magic the gathering deck"
+    //      Where you can build different types of decks.
+    //public Object DeckStats;
+    
+}//FUNC::END
