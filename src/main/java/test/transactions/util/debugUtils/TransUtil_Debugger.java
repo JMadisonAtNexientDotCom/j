0b49cef42282b27fb5c8a1ac3043dc9072238141
John Mark Isaac Madison <JMadison@Nexient.com>
2015-09-16 14:15:56 -0400

    WORKING_COMMIT. Saftey Save

--- src/main/java/test/transactions/util/debugUtils/TransUtil_Debugger.java
+++ src/main/java/test/transactions/util/debugUtils/TransUtil_Debugger.java
@@ -2,11 +2,11 @@ package test.transactions.util.debugUtils;
 
 import test.transactions.util.TransUtil;
 
-/**
+/**-----------------------------------------------------------------------------
  * A debug class for TransUtil.java. I have some questions about concurrency
  * that I need answered. I've read up on it. But it is time to do some actual
  * tests to see what is really going on.
- * @author jmadison                                                       **/
+ * @author jmadison      ----------------------------------------------------**/
 public class TransUtil_Debugger {
     
     /** Have multiple API calls on server call this method. **/
@@ -14,11 +14,11 @@ public class TransUtil_Debugger {
         TransUtil.debugSharedThreadCounter++;
     }//FUNC::END
     
-    /** Have API call on server that calls this method and sees
+    /** Have API call on server that calls this method and sees ----------------
      *  if it's result is the total sum of all incrementSharedCounter()
      *  calls that were executed on all threads.
      * @return : Value of variable I believe to be shared between multiple 
-                 threads.                                                    **/
+                 threads.  --------------------------------------------------**/
     public static int getSharedCounterValue(){
         return TransUtil.debugSharedThreadCounter;
     }//FUNC::END
