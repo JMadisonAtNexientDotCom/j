ae617bb3ec5ef22767c37b8e014d6dce36c33869
John Mark Isaac Madison <JMadison@Nexient.com>
2015-09-19 21:54:10 -0400

    BUG_FIX: Allowing Content-Type header forREST

--- src/main/java/test/servlets/corsFiltering/MyCorsFilter.java
+++ src/main/java/test/servlets/corsFiltering/MyCorsFilter.java
@@ -32,12 +32,21 @@ public class MyCorsFilter implements Filter {
         String accessSlug = "Access-Control-Allow-Origin";
       ((HttpServletResponse)response).addHeader(accessSlug, "*");
         
-        
+        //MIGHT NOT BE NEEDED!!! I think I forgot to push to server before
+        //I tried my HTTP POST.
+        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         //Added to make HTTP POST work:
         //http://www.codingpedia.org/ama/how-to-add-cors-support-on-the-server-side-in-java-with-jersey/
         String methodSlug = "Access-Control-Allow-Methods";
         String allowedMethods = "GET, POST, DELETE, PUT";
         ((HttpServletResponse)response).addHeader(methodSlug, allowedMethods);
+        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        
+        //Fixing this error:
+        //Request header field Content-Type is not allowed by Access-Control-Allow-Headers
+        String headerSlug = "Access-Control-Allow-Headers";
+        String allowedHeaders = "Content-Type";
+        ((HttpServletResponse)response).addHeader(headerSlug, allowedHeaders);
      
         //Huntch that this belongs below all of the header setting:
         chain.doFilter(request, response);
