3c1ce5e74599789de808da2993c4ee96dce8e39c
John Mark Isaac Madison <JMadison@Nexient.com>
2015-09-25 13:04:37 -0400

    Attempting to get resource.

--- src/main/java/frontEndBackEndIntegration/childComponents/libInject/LibraryInjection.java
+++ src/main/java/frontEndBackEndIntegration/childComponents/libInject/LibraryInjection.java
@@ -5,7 +5,14 @@
  */
 package frontEndBackEndIntegration.childComponents.libInject;
 
-import test.servlets.rest.utilityServlets.FileContentFetcher;
+//import test.servlets.rest.utilityServlets.FileContentFetcher;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import test.config.constants.ResourceRelativeFolderPaths;
+
 
 /**
  *
@@ -30,11 +37,39 @@ public class LibraryInjection {
      * @return :A series of formatted javascript library tags for HTML/JSP docs
      */
     public static String getLibTagsJS(){
-        String tags = "";
-        tags +=  "<!-- todo, JS importing code -->";
-    
+        //String tags = "";
+        //tags +=  "<!-- todo, JS importing code -->";
+        String relativeFolder = ResourceRelativeFolderPaths.HTML_INJECT;
+        String relativePath = relativeFolder + "/" + "JSLibs.html";
+        InputStream stream;
+        stream = LibraryInjection.class.getResourceAsStream(relativePath);
         
-        return tags;
+        BufferedReader reader;
+        reader = new BufferedReader(new InputStreamReader(stream));
+        String docText = "Was never set";
+        try{
+            String newLine = System.lineSeparator();
+            //To convert to string, use a string builder:
+            //http://stackoverflow.com/questions/4666748/
+            //                            java-how-to-read-bufferedreader-faster
+            //SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
+            StringBuilder builder = new StringBuilder();
+            String aux;
+            try{
+               while ((aux = reader.readLine()) != null) {
+                    builder.append(aux);
+                    builder.append(newLine); //get your line endings back.
+               }//NEXT
+               docText = builder.toString();
+            }catch(IOException e){
+                docText = "getJsonText Failed! e==" + e.toString();
+            }//TRY::END
+            //SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
+        }catch(Exception e){
+            return "String builder block failed. Exception:" + e.toString();
+        }
+        
+        return docText;
     }//FUNC::END
     
 }//CLASS::END
