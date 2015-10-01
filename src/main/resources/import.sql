c7e1a9bb66e1fe4e6abefdac8693b2940a1f8943
John Mark Isaac Madison <JMadison@Nexient.com>
2015-09-30 20:56:43 -0400

    UNKNOWN COLUMN == Import.sql create table error?

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -51,7 +51,7 @@ DROP TABLE IF EXISTS owner_table; -- JOINS: { token_table(id), ninja_table(id), 
 -- Max length == 800, as in: 10 lines max when each line is 80 chars. --
 -- Max length == 80 (one line) for our answers (rhymes).              --
 -- -----------------------------------| [B][B][B][B][B] BASE ENTITY FIELDS [B][B][B][B][B][B][B][B][B][B][B][B][B][B][B][B][B][B][B][B][B][B][B][B][B][B][B][B][B][B][B][B]-------|
-CREATE TABLE token_table (id serial PRIMARY KEY, token_hash varchar(80));
+CREATE TABLE token_table (id serial PRIMARY KEY, dele BOOLEAN, comment varchar(80), global_save_id INT UNSIGNED, record_local_save_count INT UNSIGNED, token_hash varchar(80));
 CREATE TABLE ninja_table (id serial PRIMARY KEY, dele BOOLEAN, comment varchar(80), global_save_id INT UNSIGNED, record_local_save_count INT UNSIGNED, name varchar(80), phone INT UNSIGNED, email varchar(80), portfolio_url varchar(80) );
 CREATE TABLE riddle_table (id serial PRIMARY KEY, text varchar(800) );
 CREATE TABLE rhyme_table (id serial PRIMARY KEY, text varchar(80) );
