3bd85e961a15f23242a6fce1b2009caa634c36f4
John Mark Isaac Madison <JMadison@Nexient.com>
2015-10-20 16:15:00 -0400

    Fixed broken build.

--- src/main/java/test/transactions/util/tables/ink/InkPersistUtil.java
+++ src/main/java/test/transactions/util/tables/ink/InkPersistUtil.java
@@ -81,7 +81,7 @@ public class InkPersistUtil {
                 curInk = (InkPurse)genericEnt;
                 
                 if(null == curInk){doError("[cast resulted in null object]");}
-                if(null == curInk.group_id){doError("[group_id is null!]");}
+                //if(null == curInk.group_id){doError("[group_id is null!]");}
                 long group_id = curInk.group_id;
                 currentCheckSum = GroupTable.getChecksumOfID(group_id);
                 if(currentCheckSum == numQuips){
